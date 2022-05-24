bb9d768ab53e071a90f68fb5d6ad4a5902df75b7
Arturo <adiaz0511@hotmail.com>
2022-05-24 18:54:27 -0500

    Added Comments

--- iOS_Challenge_iTunes/Models/Styles.swift
+++ iOS_Challenge_iTunes/Models/Styles.swift
@@ -26,7 +26,7 @@ extension View {
     }
 }
 
-// ButtonStyle for a bouncy button with configurable background and shape radius
+// ButtonStyle for a bouncy button with configurable background and radius
 struct BouncyButton: ButtonStyle {
     var color: LinearGradient
     var cornerRadius: CGFloat
