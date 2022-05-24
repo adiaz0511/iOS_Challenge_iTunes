eb60823062f80d75be310ff4b1bb150a7590224c
Arturo <adiaz0511@hotmail.com>
2022-05-24 16:28:36 -0500

    Initial Commit

---
+++ iOS_Challenge_iTunes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOS_Challenge_iTunes
+//
+//  Created by Arturo Diaz on 24/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOS_Challenge_iTunes/iOS_Challenge_iTunesApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Challenge_iTunesApp.swift
+//  iOS_Challenge_iTunes
+//
+//  Created by Arturo Diaz on 24/05/22.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Challenge_iTunesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
